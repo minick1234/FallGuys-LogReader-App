29e19fecdf8f4f74c24b475c828284ecb1d9d534
Dominik Ziolkowski <minickisbeast@gmail.com>
2023-09-26 22:37:52 -0400

    Organized Folders
    Organized the folder structure for this app.

---
+++ FallGodsLogReader/src/Match.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+/*
+
+
+ */
+class Match {
+    ArrayList<Round> rounds = new ArrayList<>();
+
+    boolean matchFinished = false;
+    boolean matchWon = false;
+
+    int numOfRounds;
+
+    public Match() {
+
+    }
+
+
+}
